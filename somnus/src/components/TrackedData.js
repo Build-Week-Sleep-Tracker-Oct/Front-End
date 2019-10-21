b473586c7ed091a0a040a4a788e4a91aeb335c6f
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-21 14:36:18 -0700

    Rearranged component layout and added pathing for individual sleep entries

---
+++ somnus/src/components/TrackedData.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Loader from 'react-loader-spinner'
+
+const TrackedData = props => {
+    const sleepItem = props.data.find(
+        item => `${item.id}` === props.match.params.id
+    )
+
+    if(!sleepItem) {
+        return <Loader type="Rings" color="#00BFFF" height={100} width={100} /> 
+    }
+    
+    console.log(sleepItem)
+    return (
+        <>
+            <div key={sleepItem.id}>
+                <p>{sleepItem.date}</p>
+                <p>{sleepItem.feels}</p>
+                <p>{sleepItem.notes}</p>
+                <p>{sleepItem.timeFrom}</p>
+                <p>{sleepItem.timeTo}</p>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </>
+    )
+}
+
+const mapStatetoProps = state => {
+    return {
+        data: state.data,
+        isFetching: state.isFetching,
+        isPosting: state.isPosting,
+        error: state.error
+    }
+  }
+  
+
+export default connect(
+    mapStatetoProps,
+    {  }
+)(TrackedData)
+
+// {Date(props.item.date).slice(0, 24)}
