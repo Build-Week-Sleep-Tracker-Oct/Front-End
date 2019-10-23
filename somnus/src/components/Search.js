f3c14abe94812ef0651c8cb9f32c69ee453ec9ba
AustinHadden <vitovalletori@gmail.com>
2019-10-23 18:27:26 -0500

    fixing some search bugs

--- somnus/src/components/Search.js
+++ somnus/src/components/Search.js
@@ -6,21 +6,28 @@ const Search = (props) => {
   console.log(props.data)
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [start, setStart] = useState([]);
+  //const [start, setStart] = useState([]);
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get("/api/user/sleepdata")
-      .then(response => {
-        //console.log(response);
-        setStart(response.data);
-        const data = response.data.filter(mood =>
-          mood.feels.includes(searchTerm)
-        );
+//   useEffect(() => {
+//     axiosWithAuth()
+//       .get("/api/user/sleepdata")
+//       .then(response => {
+//         //console.log(response);
+//         //setStart(response.data);
+        
+//         );
+        
+//       })
+//       .catch(error => console.log(error));
+//   }, [searchTerm]);
+
+useEffect(() => {
+    const data = props.data.filter(mood =>
+        mood.feels.includes(searchTerm))
         setSearchResults(data);
-      })
-      .catch(error => console.log(error));
-  }, [searchTerm]);
+}, [searchTerm]);
+
+  
 
   const handleChange = event => {
     setSearchTerm(event.target.value);
@@ -52,57 +59,57 @@ if(!props.data) {
       </form>
 
       {
-        // searchResults.length === 0
-        // ? start.map(item => {
-        //     if (Number(item.feels) === 1) {
-        //       return (
-        //         <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
-        //           <p>😡</p>
-        //           <p>{item.notes}</p>
-        //           <p>{item.dateTimeFrom}</p>
-        //           <p>{item.dateTimeTo}</p>
-        //         </div>
-        //       );
-        //     } else if (Number(item.feels) === 2) {
-        //       return (
-        //         <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
-        //           <p>😭</p>
-        //           <p>{item.notes}</p>
-        //           <p>{item.dateTimeFrom}</p>
-        //           <p>{item.dateTimeTo}</p>
-        //         </div>
-        //       );
-        //     } else if (Number(item.feels) === 3) {
-        //       return (
-        //         <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
-        //           <p>😐</p>
-        //           <p>{item.notes}</p>
-        //           <p>{item.dateTimeFrom}</p>
-        //           <p>{item.dateTimeTo}</p>
-        //         </div>
-        //       );
-        //     } else if (Number(item.feels) === 4) {
-        //       return (
-        //         <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
-        //           <p>😀</p>
-        //           <p>{item.notes}</p>
-        //           <p>{item.dateTimeFrom}</p>
-        //           <p>{item.dateTimeTo}</p>
-        //         </div>
-        //       );
-        //     } else {
-        //       return (
-        //         <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
-        //           <p>{item.feels}</p>
-        //           <p>{item.notes}</p>
-        //           <p>{item.dateTimeFrom}</p>
-        //           <p>{item.dateTimeTo}</p>
-        //         </div>
-        //       );
-        //     }
-        //   })
-        // : 
-        props.data.map(item => {
+        searchResults.length === 0
+        ? props.data.map(item => {
+            if (Number(item.feels) === 1) {
+              return (
+                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                  <p>😡</p>
+                  <p>{item.notes}</p>
+                  <p>{item.dateTimeFrom}</p>
+                  <p>{item.dateTimeTo}</p>
+                </div>
+              );
+            } else if (Number(item.feels) === 2) {
+              return (
+                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                  <p>😭</p>
+                  <p>{item.notes}</p>
+                  <p>{item.dateTimeFrom}</p>
+                  <p>{item.dateTimeTo}</p>
+                </div>
+              );
+            } else if (Number(item.feels) === 3) {
+              return (
+                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                  <p>😐</p>
+                  <p>{item.notes}</p>
+                  <p>{item.dateTimeFrom}</p>
+                  <p>{item.dateTimeTo}</p>
+                </div>
+              );
+            } else if (Number(item.feels) === 4) {
+              return (
+                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                  <p>😀</p>
+                  <p>{item.notes}</p>
+                  <p>{item.dateTimeFrom}</p>
+                  <p>{item.dateTimeTo}</p>
+                </div>
+              );
+            } else {
+              return (
+                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                  <p>{item.feels}</p>
+                  <p>{item.notes}</p>
+                  <p>{item.dateTimeFrom}</p>
+                  <p>{item.dateTimeTo}</p>
+                </div>
+              );
+            }
+          })
+        : 
+        searchResults.map(item => {
             if (Number(item.feels) === 1) {
               return (
                 <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
