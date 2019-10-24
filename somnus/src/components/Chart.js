7fade0c3d58f14d8b973ac1590d5e9b8b7e4a47f
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-24 16:09:17 -0700

    Styled chart

--- somnus/src/components/Chart.js
+++ somnus/src/components/Chart.js
@@ -65,16 +65,17 @@ const Chart = props => {
         labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
         datasets: [{
                 label: 'Time Slept',
-                borderColor: 'purple',
+                borderColor: 'white',
                 data: nightlyData
             },{
                 label: 'Feels',
-                borderColor: 'black',
+                borderColor: 'light grey',
                 data: feelingNumber
             }]
     }
 
     const options = {
+      maintainAspectRation: false,
       scales: {
         yAxes: [{
           scaleLabel: {
@@ -96,8 +97,8 @@ const Chart = props => {
     return (
         <div className='chart'>
             <Line
-            width={100}
-            height={30} 
+            width={125}
+            height={100} 
             options={options}
             data={data} />
         </div>
--- somnus/src/components/TrackerList.js
+++ somnus/src/components/TrackerList.js
@@ -40,61 +40,61 @@ const TrackerList = props => {
     return (
         <div className="tracker-list">
             <div className="tracker-content">
-            <Chart />
-            <Alarm />
-                {props.isFetching ? 
-                <Loader
-                    type="Rings"
-                    color="#00BFFF"
-                    height={100}
-                    width={100}
-                /> 
-                : ''}
-            <p className='avgFeel'>{feelAvg === 4 ? 'Your average sleep is 😀' : ''}</p>
-            <p className='avgFeel'>{feelAvg >= 3 && feelAvg < 4 ? 'Your average sleep is 😐' : ''}</p>
-            <p className='avgFeel'>{feelAvg >= 2 && feelAvg < 3 ? 'Your average sleep is 😭' : ''}</p>
-            <p className='avgFeel'>{feelAvg >= 1 && feelAvg < 2 ? 'Your average sleep is 😡' : ''}</p>
-            <button className='searchButton' onClick={() => props.history.push('/search')}>Search</button>
-            {props.data.map(item => {
-                if(Number(item.feels) === 1){
-                    return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
-                                <p>😡</p>
-                                <p>{item.notes}</p>
-                                <p>{item.dateTimeFrom}</p>
-                                <p>{item.dateTimeTo}</p>
-                            </div>
-                } else if(Number(item.feels) === 2) {
-                    return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
-                                <p>😭</p>
-                                <p>{item.notes}</p>
-                                <p>{item.dateTimeFrom}</p>
-                                <p>{item.dateTimeTo}</p>
-                            </div>
-                } else if(Number(item.feels) === 3) {
-                    return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
-                                <p>😐</p>
-                                <p>{item.notes}</p>
-                                <p>{item.dateTimeFrom}</p>
-                                <p>{item.dateTimeTo}</p>
-                            </div>
-                } else if(Number(item.feels) === 4) {
-                    return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
-                                <p>😀</p>
-                                <p>{item.notes}</p>
-                                <p>{item.dateTimeFrom}</p>
-                                <p>{item.dateTimeTo}</p>
-                            </div>
-                } else {
-                    return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
-                                <p>{item.feels}</p>
-                                <p>{item.notes}</p>
-                                <p>{item.dateTimeFrom}</p>
-                                <p>{item.dateTimeTo}</p>
-                            </div>
+                <Chart />
+                <Alarm />
+                    {props.isFetching ? 
+                    <Loader
+                        type="Rings"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    /> 
+                    : ''}
+                <p className='avgFeel'>{feelAvg === 4 ? 'Your average sleep is 😀' : ''}</p>
+                <p className='avgFeel'>{feelAvg >= 3 && feelAvg < 4 ? 'Your average sleep is 😐' : ''}</p>
+                <p className='avgFeel'>{feelAvg >= 2 && feelAvg < 3 ? 'Your average sleep is 😭' : ''}</p>
+                <p className='avgFeel'>{feelAvg >= 1 && feelAvg < 2 ? 'Your average sleep is 😡' : ''}</p>
+                <button className='searchButton' onClick={() => props.history.push('/search')}>Search</button>
+                {props.data.map(item => {
+                    if(Number(item.feels) === 1){
+                        return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
+                                    <p>😡</p>
+                                    <p>{item.notes}</p>
+                                    <p>{item.dateTimeFrom}</p>
+                                    <p>{item.dateTimeTo}</p>
+                                </div>
+                    } else if(Number(item.feels) === 2) {
+                        return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
+                                    <p>😭</p>
+                                    <p>{item.notes}</p>
+                                    <p>{item.dateTimeFrom}</p>
+                                    <p>{item.dateTimeTo}</p>
+                                </div>
+                    } else if(Number(item.feels) === 3) {
+                        return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
+                                    <p>😐</p>
+                                    <p>{item.notes}</p>
+                                    <p>{item.dateTimeFrom}</p>
+                                    <p>{item.dateTimeTo}</p>
+                                </div>
+                    } else if(Number(item.feels) === 4) {
+                        return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
+                                    <p>😀</p>
+                                    <p>{item.notes}</p>
+                                    <p>{item.dateTimeFrom}</p>
+                                    <p>{item.dateTimeTo}</p>
+                                </div>
+                    } else {
+                        return <div className='entry' key={item.id} onClick={e => entryRoute(e, item)}>
+                                    <p>{item.feels}</p>
+                                    <p>{item.notes}</p>
+                                    <p>{item.dateTimeFrom}</p>
+                                    <p>{item.dateTimeTo}</p>
+                                </div>
 
-                }
-            })}
-            <button className='addEntryButton' onClick={() => props.history.push('/sleepentry')}>Add Entry</button>
+                    }
+                })}
+                <button className='addEntryButton' onClick={() => props.history.push('/sleepentry')}>Add Entry</button>
             </div>
         </div>
     )
