c5b975294fe5bed5539b213d6b70f7afc5cfd83f
AustinHadden <vitovalletori@gmail.com>
2019-10-24 12:44:54 -0500

    fixed the search function

--- somnus/src/components/Search.js
+++ somnus/src/components/Search.js
@@ -1,62 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
-import Loader from 'react-loader-spinner';
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import Loader from "react-loader-spinner";
+import { NavLink } from "react-router-dom";
 
 const Search = props => {
-	//console.log(props.data);
-	const [searchTerm, setSearchTerm] = useState('');
-	const [searchResults, setSearchResults] = useState([]);
-	const [start, setStart] = useState([]);
+  //console.log(props);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [start, setStart] = useState([]);
 
-	useEffect(() => {
-		axiosWithAuth()
-			.get('/api/users/sleepdata')
-			.then(response => {
-				//console.log(response);
-				setStart(response.data);
-				const data = response.data.filter(mood =>
-					mood.feels.includes(searchTerm),
-				);
-				setSearchResults(data);
-			})
-			.catch(error => console.log(error));
-	}, [searchTerm]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/users/sleepdata")
+      .then(response => {
+        //console.log(response);
+        setStart(response.data);
+        const data = response.data.filter(mood =>
+          mood.feels.includes(searchTerm)
+        );
+        setSearchResults(data);
+      })
+      .catch(error => console.log(error));
+  }, [searchTerm]);
 
-	const handleChange = event => {
-		setSearchTerm(event.target.value);
-	};
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
-	const entryRoute = (e, item) => {
-		e.preventDefault();
-		props.history.push(`/trackerlist/${item.id}`);
-	};
+  const entryRoute = (e, item) => {
+    e.preventDefault();
+    props.history.push(`/trackerlist/${item.id}`);
+  };
 
-	if (!props.data) {
-		return <Loader type='Rings' color='#00BFFF' height={100} width={100} />;
-	}
+  if (!props.data) {
+    return <Loader type="Rings" color="#00BFFF" height={100} width={100} />;
+  }
 
-	return (
-		<section className='search-form'>
-			<form className='search'>
-				<select
-					name='search-by-feels'
-					onChange={e => handleChange(e)}
-					value={searchTerm}
-				>
-					<option value=''>Search by mood</option>
-					<option value='4'>😀</option>
-					<option value='3'>😐</option>
-					<option value='2'>😭</option>
-					<option value='1'>😡</option>
-				</select>
-			</form>
+  return (
+    <section className="search-form">
+      <form className="search">
+        <select
+          name="search-by-feels"
+          onChange={e => handleChange(e)}
+          value={searchTerm}
+        >
+          <option value="">Search by mood</option>
+          <option value="4">😀</option>
+          <option value="3">😐</option>
+          <option value="2">😭</option>
+          <option value="1">😡</option>
+        </select>
+      </form>
 
-      {
-        searchResults.length === 0
+      {searchResults.length === 0
         ? start.map(item => {
             if (Number(item.feels) === 1) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😡</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -65,7 +69,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 2) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😭</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -74,7 +82,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 3) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😐</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -83,7 +95,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 4) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😀</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -92,7 +108,11 @@ const Search = props => {
               );
             } else {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>{item.feels}</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -101,11 +121,14 @@ const Search = props => {
               );
             }
           })
-        : 
-        searchResults.map(item => {
+        : searchResults.map(item => {
             if (Number(item.feels) === 1) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😡</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -114,7 +137,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 2) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😭</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -123,7 +150,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 3) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😐</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -132,7 +163,11 @@ const Search = props => {
               );
             } else if (Number(item.feels) === 4) {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>😀</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -141,7 +176,11 @@ const Search = props => {
               );
             } else {
               return (
-                <div className="entry" key={item.id} onClick={e => entryRoute(e, item)}>
+                <div
+                  className="entry"
+                  key={item.id}
+                  onClick={e => entryRoute(e, item)}
+                >
                   <p>{item.feels}</p>
                   <p>{item.notes}</p>
                   <p>{item.dateTimeFrom}</p>
@@ -150,6 +189,17 @@ const Search = props => {
               );
             }
           })}
+          <NavLink
+            to={"/trackerlist"}
+            style={{
+              color: `black`,
+              fontSize: `1.4rem`,
+              textDecoration: `none`,
+              textAlign: `left`
+            }}
+          >
+            Back
+          </NavLink>
     </section>
   );
 };
